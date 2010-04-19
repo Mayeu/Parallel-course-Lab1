ec9e2bf033dd680f7b8fae19f4111f089167431f
Matthieu Maury <mayeu.tik@gmail.com>
2010-04-19 23:22:00 +0200

    Split onetoall in 3 files onetoall, fanout, onetoall_glob Modify Makefile to support new files

--- code/fanout.c
+++ code/fanout.c
@@ -16,15 +16,20 @@ int main(int argc, char *argv[]) {
   MPI_Init(&argc, &argv);               /* Initialize MPI               */
   MPI_Comm_size(MPI_COMM_WORLD, &size); /* Get the number of processors */
   MPI_Comm_rank(MPI_COMM_WORLD, &rank); /* Get my number                */
-
-  /* Pass on */
+  
+  /* Fan out */
   a = 999.999;
-  if(rank != 0) //the first processor does not receive
-    MPI_Recv(&a,1,MPI_DOUBLE,rank-1,rank-1,MPI_COMM_WORLD,&status);
-  if(rank != size -1) //the last processor does not send
-    MPI_Send(&a,1,MPI_DOUBLE,rank+1,rank,MPI_COMM_WORLD);
-  if(rank != 0)
-    printf("Processor %d got %f with label %d\n", rank,a,rank-1);
+  int k = ceil(log2(size));
+  int distance;
+  for(i=0;i<k;i++){
+    distance = pow(2,i);
+    if(rank < distance && rank+distance < size)
+      MPI_Send(&a,1,MPI_DOUBLE,rank+distance,rank,MPI_COMM_WORLD);
+    else if(rank < 2*distance && rank-distance >= 0){
+      MPI_Recv(&a,1,MPI_DOUBLE,rank-distance,rank-distance,MPI_COMM_WORLD,&status);
+      printf("Processor %d got %f from %d\n",rank,a,rank-distance);
+    }
+  }
 
   MPI_Finalize(); 
 
--- code/onetoall_glob.c
+++ code/onetoall_glob.c
@@ -17,14 +17,10 @@ int main(int argc, char *argv[]) {
   MPI_Comm_size(MPI_COMM_WORLD, &size); /* Get the number of processors */
   MPI_Comm_rank(MPI_COMM_WORLD, &rank); /* Get my number                */
 
-  /* Pass on */
+  /* Broadcast */
   a = 999.999;
-  if(rank != 0) //the first processor does not receive
-    MPI_Recv(&a,1,MPI_DOUBLE,rank-1,rank-1,MPI_COMM_WORLD,&status);
-  if(rank != size -1) //the last processor does not send
-    MPI_Send(&a,1,MPI_DOUBLE,rank+1,rank,MPI_COMM_WORLD);
-  if(rank != 0)
-    printf("Processor %d got %f with label %d\n", rank,a,rank-1);
+  MPI_Bcast(&a,1,MPI_DOUBLE,0,MPI_COMM_WORLD);
+  printf("Processor %d got %f\n", rank,a);
 
   MPI_Finalize(); 
 
