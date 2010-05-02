d1c9ddafacacda34c4697ff2194361b021888983
Matthieu Maury <mayeu.tik@gmail.com>
2010-05-02 18:02:46 +0200

    Update appendix
    Add the hello.c source code

--- code/hello.c
+++ code/hello.c
@@ -8,17 +8,17 @@
 
 int main(int argc, char *argv[]) {
 
-  int tmp;
-  MPI_Init(&argc, &argv);               /* Initialize MPI               */
-  MPI_Comm_rank(MPI_COMM_WORLD,&tmp);
-  printf("My rank: %d\n",tmp);
-  if(tmp == 0){
-    MPI_Comm_size(MPI_COMM_WORLD,&tmp);
-    printf("Size: %d\n",tmp);
-  }
-  printf("Hello World!\n");             /* Print a message              */
+	int tmp;
+	MPI_Init(&argc, &argv);               /* Initialize MPI               */
+	MPI_Comm_rank(MPI_COMM_WORLD,&tmp);
+	printf("My rank: %d\n",tmp);
+	if(tmp == 0){
+		MPI_Comm_size(MPI_COMM_WORLD,&tmp);
+		printf("Size: %d\n",tmp);
+	}
+	printf("Hello World!\n");             /* Print a message              */
 
-  MPI_Finalize();                       /* Shut down and clean up MPI   */
+	MPI_Finalize();                       /* Shut down and clean up MPI   */
 
-  return 0;
+	return 0;
 }
